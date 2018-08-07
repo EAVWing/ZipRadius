87e64859657a0e2978da058faca9689d1b497eea
EAVWing <stephenewing1337@gmail.com>
2018-08-07 15:25:21 -0400

    0 errors | 0 warnings | 0 notes

--- R/ZipRadius.R
+++ R/ZipRadius.R
@@ -1,22 +1,27 @@
-library(geosphere)
+suppressWarnings(library(geosphere))
 library(magrittr)
 library(dplyr)
+library(rlang)
 
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
 
 #' The zipRadius Function
 #'
 #' @import magrittr
 #' @importFrom geosphere distHaversine
-#' @importFrom dplyr filter select rename
+#' @importFrom dplyr filter select rename %>%
+#' @importFrom utils zip
+#' @importFrom rlang .data
 #' @param zipcode the reference zip code of which you'd like the list of zip codes within a give radius as character
 #' @param radius the distance in miles from the center of the given zip to the center of the other zips as numeric
 #' @examples zipRadius("30316", 10)
 #'@export
 zipRadius <- function(zipcode, radius){
   # Get the lat/lon of the reference zip
-  refPoint <- dplyr::filter(zipList, zip == zipcode) %>%
-    dplyr::select(latitude, longitude) %>%
-    dplyr::rename(refLat = latitude, refLon = longitude)
+  refPoint <- dplyr::filter(zipList, .data$zip == zipcode) %>%
+    dplyr::select(.data$latitude, .data$longitude) %>%
+    dplyr::rename(refLat = .data$latitude, refLon = .data$longitude)
 
   # Add the lat/lon of the ref zip to the zipList
   zipList <- cbind(zipList, refPoint)
@@ -33,8 +38,8 @@ zipRadius <- function(zipcode, radius){
   zipList$Distance <- round(distance, 2)
 
   # Remove reference lat/lon and filter down to below selected radius
-  zipList <- dplyr::select(zipList, -refLat, -refLon) %>%
-    dplyr::filter(distance < radius)
+  zipList <- dplyr::select(zipList, -.data$refLat, -.data$refLon) %>%
+    dplyr::filter(.data$Distance < radius)
 
   return(zipList)
   }
