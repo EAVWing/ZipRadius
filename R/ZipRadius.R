7769eb7c6d4d2ea06e64db0460f5d237c1b085c3
EAVWing <stephenewing1337@gmail.com>
2018-08-08 16:17:39 -0400

    add getStates and getZipPop functions to play nice with choroplethrZip

--- R/ZipRadius.R
+++ R/ZipRadius.R
@@ -1,21 +1,26 @@
-suppressWarnings(library(geosphere))
+library(geosphere)
 library(magrittr)
 library(dplyr)
 library(rlang)
 
-## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
-
 #' The zipRadius Function
 #'
 #' @import magrittr
+#'
 #' @importFrom geosphere distHaversine
+#'
 #' @importFrom dplyr filter select rename %>%
+#'
 #' @importFrom utils zip
+#'
 #' @importFrom rlang .data
+#'
 #' @param zipcode the reference zip code of which you'd like the list of zip codes within a give radius as character
+#'
 #' @param radius the distance in miles from the center of the given zip to the center of the other zips as numeric
+#'
 #' @examples zipRadius("30316", 10)
+#'
 #'@export
 zipRadius <- function(zipcode, radius){
   # Get the lat/lon of the reference zip
@@ -44,7 +49,37 @@ zipRadius <- function(zipcode, radius){
   return(zipList)
   }
 
+#' getStates returns the list of states which have zip codes which fall in a specified radius in lower case format for use in choroplethrZip
+#'
+#' @param zipcode the reference zip code of which you'd like the list of zip codes within a give radius as character
+#'
+#' @param radius the distance in miles from the center of the given zip to the center of the other zips as numeric
+#'
+#' @examples getStates("30316", 10)
+#'
+#'@export
+getStates <- function(zipcode, radius){
+  data <- zipRadius(zipcode, radius)
+  stateTable <- as.data.frame(cbind(datasets::state.abb, tolower(datasets::state.name)))
+  zipStates <- unique(data$state)
+  States <- as.character(stateTable[which(stateTable$V1 %in% zipStates),2])
+  States
+}
 
-
-
-
+#' getZipPop returns a data frame of zipcodes and their population where the zip codes fall within a given radius for use in choroplethrZip
+#'
+#' @importFrom dplyr data_frame inner_join
+#'
+#' @param zipcode the reference zip code of which you'd like the list of zip codes within a give radius as character
+#'
+#' @param radius the distance in miles from the center of the given zip to the center of the other zips as numeric
+#'
+#' @examples getStates("30316", 10)
+#'
+#'@export
+getZipPop <- function(zipcode, radius){
+  data <- zipRadius(zipcode, radius)
+  zips <- dplyr::data_frame(region = data[,1]) %>%
+    dplyr::inner_join(df_pop_zip)
+  zips
+  }
--- vignettes/ZipRadius.R
+++ vignettes/ZipRadius.R
@@ -4,45 +4,7 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ---- include=FALSE------------------------------------------------------
-library(zipcode)
-library(geosphere)
-library(magrittr)
-library(dplyr)
-
-data(zipcode)
-zipList <- zipcode
-rm(zipcode)
-
-zipRadius <- function(zipcode, radius){
-
-  # Get the lat/lon of the reference zip
-  refPoint <- dplyr::filter(zipList, zip == zipcode) %>%
-    dplyr::select(latitude, longitude) %>%
-    dplyr::rename(refLat = latitude, refLon = longitude)
-
-  # Add the lat/lon of the ref zip to the zipList
-  zipList <- cbind(zipList, refPoint)
-
-  # Radius of Earth in miles to adjust for km
-  r = 3959
-
-  # Creating Table of the coordinates. Makes it easy to calc distance
-  Points1 <- cbind(zipList$longitude,zipList$latitude)
-  Points2 <- cbind(zipList$refLon,zipList$refLat)
-  distance <- distHaversine(Points1,Points2,r)
-
-  # Adding distance back on to the original zipList
-  zipList$Distance <- round(distance, 2)
-
-  # Remove reference lat/lon and filter down to below selected radius
-  zipList <- dplyr::select(zipList, -refLat, -refLon) %>%
-    dplyr::filter(distance < radius)
-
-  return(zipList)
-  }
-
-## ---- warning=FALSE------------------------------------------------------
-test <- zipRadius("30316", 10)
-head(test)
+## ---- eval=FALSE---------------------------------------------------------
+#  test <- zipRadius("30316", 10)
+#  head(test)
 
