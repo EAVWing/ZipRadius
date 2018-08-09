9ba2fa586979f114f2c281d6718929391f56eae6
EAVWing <stephenewing1337@gmail.com>
2018-08-09 08:39:14 -0400

    updated readme

--- vignettes/ZipRadius.R
+++ vignettes/ZipRadius.R
@@ -3,8 +3,42 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
+library(ZipRadius)
+
+## ------------------------------------------------------------------------
+test <- zipRadius("30316", 3.5)
+test
 
 ## ---- eval=FALSE---------------------------------------------------------
-#  test <- zipRadius("30316", 10)
-#  head(test)
+#  library(devtools)
+#  install_github('arilamstein/choroplethrZip@v1.3.0')
+
+## ------------------------------------------------------------------------
+getStates("30316", 200)
+
+## ------------------------------------------------------------------------
+getZips("30316", 3.5)
+
+## ------------------------------------------------------------------------
+getZipPop("30316", 3.5)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  library(ZipRadius)
+#  library(choroplethrZip)
+#  library(ggplot2)
+#  
+#  makeZipMap <- function(zipcode, radius){
+#    choro = choroplethrZip::ZipChoropleth$new(getZipPop(zipcode, radius))
+#    suppressMessages(suppressWarnings(choro$prepare_map()))
+#    choro$legend = "Population"
+#    ec_zips = getZips(zipcode, radius)
+#    ec_df   = choro$choropleth.df[choro$choropleth.df$region %in% ec_zips,]
+#    ec_plot = choro$render_helper(ec_df, "", choro$theme_clean())
+#  
+#    ec_plot + ggplot2::coord_map()
+#  }
+#  makeZipMap("30316", 500)
+
+## ----pressure, echo=FALSE, out.width = '100%'----------------------------
+knitr::include_graphics("500miFrom30316.png")
 
