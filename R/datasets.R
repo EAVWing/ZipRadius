401436bef5325970797f4e9e94fb29c7f8dcb916
Richard Cotton <richierocks@gmail.com>
2020-02-07 12:27:07 -0500

    export zipList

---
+++ R/datasets.R
@@ -0,0 +1,16 @@
+#' zipList
+#'
+#' US zip codes, and their city, state, latitude, and longitude.
+#'
+#' @docType data
+#' @name zipList
+#' @format A data frame with five columns and 44336 rows.
+#' \describe{
+#' \item{zip}{Character. Five-digit US zip codes.}
+#' \item{city}{Character. The cities containing the zip codes.}
+#' \item{state}{Character. The states containing the zip codes.}
+#' \item{latitude}{Numeric. The latitudes of the centers of the zip codes.}
+#' \item{longitude}{Numeric. The longitudes of the centers of the zip codes.}
+#' }
+#' @export
+NULL
